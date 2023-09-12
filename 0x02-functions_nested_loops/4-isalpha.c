b053d5628661d986467799680686f7d6dc5715d9
kirengamartial <kirengamartial@gmail.com>
2023-09-12 07:22:13 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,8 @@
  */
 int _isalpha(int c)
 {
-    /* Check if c is a letter (lowercase or uppercase) */
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    
-    return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	
+	return (0);
 }
