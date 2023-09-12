039b0c8a1638b41eb4599f4cc7c2f92b874f1451
kirengamartial <kirengamartial@gmail.com>
2023-09-12 07:41:28 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    last_digit = n % 10;
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (n < 0)
+		n = -n;
 
-    _putchar('0' + last_digit);
-    return (last_digit);
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+
+	return (last_digit);
 }
