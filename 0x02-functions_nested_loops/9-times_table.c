ad6cde7addd15071104f86f36a68dc2647b90f98
kirengamartial <kirengamartial@gmail.com>
2023-09-12 08:04:27 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,26 +6,26 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            if (j == 0)
-                putchar(result + '0');
-            else
-            {
-                putchar(',');
-                putchar(' ');
-                if (result < 10)
-                    putchar(' ');
-                else
-                    putchar(result / 10 + '0');
-                putchar(result % 10 + '0');
-            }
-        }
-        putchar('\n');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				putchar(result + '0');
+			else
+			{
+				putchar(',');
+				putchar(' ');
+				if (result < 10)
+					putchar(' ');
+				else
+					putchar(result / 10 + '0');
+				putchar(result % 10 + '0');
+			}
+		}
+		putchar('\n');
+	}
 }
