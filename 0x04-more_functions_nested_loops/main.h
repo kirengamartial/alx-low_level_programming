d996572152fc9c75d3e2997d2bee3245ec870af0
kirengamartial <kirengamartial@gmail.com>
2023-09-14 03:42:49 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
 int _isupper(int c);
 
 #endif /* MAIN_H */
