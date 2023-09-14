a44556256085866cd995d0637eabb54e0f5b41a9
kirengamartial <kirengamartial@gmail.com>
2023-09-14 09:34:24 -0700

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,16 +9,16 @@
  */
 void positive_or_negative(int i)
 {
-    if (i > 0)
-    {
-        custom_puts("Positive\n");
-    }
-    else if (i < 0)
-    {
-        custom_puts("Negative\n");
-    }
-    else
-    {
-        custom_puts("Zero\n");
-    }
+	if (i > 0)
+	{
+		custom_puts("Positive\n");
+	}
+	else if (i < 0)
+	{
+		custom_puts("Negative\n");
+	}
+	else
+	{
+		custom_puts("Zero\n");
+	}
 }
