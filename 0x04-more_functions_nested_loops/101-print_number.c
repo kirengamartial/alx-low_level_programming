ab4274f1066e2c7ce9c0305aa0ec38323a30fca9
kirengamartial <kirengamartial@gmail.com>
2023-09-14 08:28:10 -0700

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	for (i = n; i > 9 || i < -9; i /= 10)
+		divisor *= 10;
+
+	for (; divisor >= 1 || divisor <= -1; divisor /= 10)
+	{
+		int digit = n / divisor;
+
+		if (digit < 0)
+			digit *= -1;
+
+		_putchar(digit + '0');
+		n -= digit * divisor;
+	}
+}
