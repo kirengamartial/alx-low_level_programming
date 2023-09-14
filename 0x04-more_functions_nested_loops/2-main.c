32757b7774895c506985d76efc3e8b8cd4f4cfd5
kirengamartial <kirengamartial@gmail.com>
2023-09-14 04:18:27 -0700

    2-mul

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,16 +8,7 @@
  */
 int main(void)
 {
-    int result1, result2;
-
-    result1 = mul(98, 1024);
-    result2 = mul(-402, 4096);
-
-    /* Call your custom print_number function to display the results */
-    print_number(result1);
-    print_char('\n');
-    print_number(result2);
-    print_char('\n');
-
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
     return (0);
 }
