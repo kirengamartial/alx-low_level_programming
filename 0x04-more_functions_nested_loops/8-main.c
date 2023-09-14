2d16c573bd67e2600bd1efd03dfa19d59d7c8c35
kirengamartial <kirengamartial@gmail.com>
2023-09-14 06:11:57 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
     print_square(2);
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of a given size
- * @size: size of the square
+ * print_square - prints a square of a given size using #
+ * @size: the size of the square
  */
 void print_square(int size)
 {
