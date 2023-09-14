398f13e9ca89d527f0fa7c6c6be6f3c6ac1d73f6
kirengamartial <kirengamartial@gmail.com>
2023-09-14 11:11:00 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
-
+ * main - Entry point of the program
+ *
+ * Description: This function causes an infinite loop.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int i;
@@ -13,6 +15,10 @@ int main(void)
 
     i = 0;
 
+    /*
+     * The code below is commented out
+     * to avoid an infinite loop.
+     */
     /* while (i < 10)
     {
         putchar(i);
