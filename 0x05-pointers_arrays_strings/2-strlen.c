aa53a73b8c6ba6724ec9081c802e682740535d8b
kirengamartial <kirengamartial@gmail.com>
2023-09-18 08:17:44 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * _strlen - returns the length of a string
+  * @s: the string whose length is being evaluated
+  * Return: a positive integer
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
