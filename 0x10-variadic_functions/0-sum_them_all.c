02caab9c290b1e497c03d0805a35521c2a0f3544
kirengamartial <kirengamartial@gmail.com>
2023-10-12 06:46:58 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,19 @@
-#include "variadic_functions.h"
-/**
- * sum_them_all - Sums all its parameters
- * @n: no of variable arguments - 1
- * Return: Sum of parameters or 0
- */
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	unsigned int sum;
-	unsigned int x;
-	va_list args;
+    va_list args;
+    int sum = 0;
+    unsigned int i;
 
-	va_start(args, n);
-	if (n == 0)
-		return (0);
-	sum = 0;
-	for (i = 0; i < n; i++)
-	{
-		x = va_arg(args, unsigned int);
-		sum = sum + x;
-	}
-	return (sum);
+    if (n == 0)
+        return 0;
+
+    va_start(args, n);
+    for (i = 0; i < n; i++) {
+        sum += va_arg(args, int);
+    }
+    va_end(args);
+
+    return sum;
 }
--- 0x10-variadic_functions/_putchar.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include "variadic_functions.h"
-
-int custom_putchar(char c)
-{
-    return write(1, &c, 1);
-}
