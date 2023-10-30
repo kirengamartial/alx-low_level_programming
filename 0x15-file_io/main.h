d3c78707757fd3e9f926eb28f322b6b42bd33dcf
kirengamartial <kirengamartial@gmail.com>
2023-10-30 05:02:39 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -9,5 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(char *message);
+void print_elf_header(Elf64_Ehdr *header);
 
 #endif
